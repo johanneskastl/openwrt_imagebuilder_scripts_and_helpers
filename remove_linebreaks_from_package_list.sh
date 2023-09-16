bae05e050f935278601d8e23b0b2e0ef1f9b94c6
Johannes Kastl <git@johannes-kastl.de>
2023-09-16 19:20:41 +0200

    ADD remove_linebreaks_from_package_list.sh

---
+++ remove_linebreaks_from_package_list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[[ $# == 1 ]] || {
+    echo "Filename as only argument is missing..."
+    exit 1
+}
+
+FILE="$1"
+OUTPUT_FILE="${FILE/_with_linebreaks/}"
+
+perl -p -e 's/\n/ /' "${FILE}" > "${OUTPUT_FILE}" || exit 11
+echo "" >> "${OUTPUT_FILE}"
