cff487cfadda8b4a394e66d66f14b5615c7f20df
Johannes Kastl <git@johannes-kastl.de>
2023-10-03 15:44:14 +0200

    download_and_build_Mikrotik_wAP_AC.sh: use download.sh script for downloading, unpacking and copying files

--- download_and_build_Mikrotik_wAP_AC.sh
+++ download_and_build_Mikrotik_wAP_AC.sh
@@ -36,34 +36,9 @@ source env_Mikrotik_wAP_AC
 }
 
 ############################################################################################################
-# Download and unpack
 #
-
-if [[ -d "./${IMAGEBUILDER_DIR_NAME}" ]]
-then
-   echo "Directory is already existing..."
-else
-    echo "Downloading files..."
-
-    [[ -f ./"${SHASUMS_FILE}" ]] || wget "${DOWNLOAD_URL}/sha256sums" -O ./"${SHASUMS_FILE}"  || exit 5
-    [[ -f "./${IMAGEBUILDER_FILE_NAME}" ]] || wget "${DOWNLOAD_URL}/${IMAGEBUILDER_FILE_NAME}" || exit 7
-
-    echo "Validating checksum..."
-    grep "${IMAGEBUILDER_FILE_NAME}" "${SHASUMS_FILE}" | sha256sum -c - || exit 11
-    
-    [[ -d "./${IMAGEBUILDER_DIR_NAME}" ]] || {
-
-        echo "Unpackaging archive"
-        tar xf "${IMAGEBUILDER_FILE_NAME}" || exit 13
-    }
-fi
-
-############################################################################################################
-# Copy files into openwrt imagebuilder directory
 #
- 
-cp -vf build.sh "./${IMAGEBUILDER_DIR_NAME}"
-cp -vf "${PACKAGE_LIST}" "./${IMAGEBUILDER_DIR_NAME}"
+./download.sh
 
 ############################################################################################################
 # Run the build inside podman
