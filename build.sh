25ec30a600cb803c3cf4c0fa9ce5893b80f25d7f
Johannes Kastl <git@johannes-kastl.de>
2023-10-15 09:18:49 +0200

    build.sh: whitespace changes

--- build.sh
+++ build.sh
@@ -39,11 +39,11 @@ apk add argp-standalone asciidoc bash bc binutils bzip2 cdrkit coreutils \
   grep gzip intltool libxslt linux-headers make musl-libintl musl-obstack-dev \
   ncurses-dev openssl-dev patch perl python3-dev rsync tar \
   unzip util-linux wget zlib-dev || exit 11
- 
+
 # missing dependency workaround (libtinfo is not installable by any APK package,
 # but can be simulated via libncurses (see: https://stackoverflow.com/a/41517423 )
 # w/o this - ERROR: package/boot/uboot-mvebu failed to build (build variant: clearfog)
-[ -e /usr/lib/libtinfo.so ] || ln -s /usr/lib/libncurses.so /usr/lib/libtinfo.so 
+[ -e /usr/lib/libtinfo.so ] || ln -s /usr/lib/libncurses.so /usr/lib/libtinfo.so
 
 echo "Packages successfully installed"
 
