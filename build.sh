634b09d6d4820236d224597894d8fa3243f96e1b
Johannes Kastl <git@johannes-kastl.de>
2024-06-01 18:21:49 +0200

    build.sh: REMOVE package installation, as everything is in the container now

--- build.sh
+++ build.sh
@@ -33,13 +33,6 @@ echo "Ready for take-off..."
 
 # https://openwrt.org/docs/guide-developer/toolchain/install-buildsystem
 
-echo "Installing packages in container..."
-apk add argp-standalone asciidoc bash bc binutils bzip2 cdrkit coreutils \
-  diffutils elfutils-dev findutils flex musl-fts-dev g++ gawk gcc gettext git \
-  grep gzip intltool libxslt linux-headers make musl-libintl musl-obstack-dev \
-  ncurses-dev openssl-dev patch perl python3-dev rsync tar \
-  unzip util-linux wget zlib-dev || exit 11
-
 # missing dependency workaround (libtinfo is not installable by any APK package,
 # but can be simulated via libncurses (see: https://stackoverflow.com/a/41517423 )
 # w/o this - ERROR: package/boot/uboot-mvebu failed to build (build variant: clearfog)
